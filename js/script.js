432ca419afe625e2aba00e4bff0927689ecb25f4
yisylvie <yisylvie1@gmail.com>
2026-04-17 20:27:44 -1000

    finished flyer

--- js/script.js
+++ js/script.js
@@ -1,27 +1,6 @@
-// check if element has been clicked or enter/space pressed on focus
-function setClickListener(el, listener) {
-    el.addEventListener("keydown", function (e) {
-        if (e.code === "Enter" || e.code === "Space") {
-            e.preventDefault();
-            listener(e);
-        }
-    });
-    el.addEventListener("click", listener);
-}
 
-// check if element has been hovered or focused
-function setHoverListener(el, listener) {
-    el.addEventListener("mouseenter", listener);
-    el.addEventListener("focus", listener);
-}
-
-// check if element has lost focus or hover
-function setUnhoverListener(el, listener) {
-    el.addEventListener("mouseleave", listener);
-    el.addEventListener("blur", listener);
-    document.addEventListener("keyup", (e) => {
-        if (e.code === "Escape" && (el == document.activeElement || el.matches(":hover"))) {
-            listener(e);
-        }
-    });
+let flyer = document.getElementsByClassName("flyer");
+let everything = document.getElementById("everything");
+for (let i = 0; i < 3; i++) {
+    everything.appendChild(flyer[0].cloneNode(true));
 }
