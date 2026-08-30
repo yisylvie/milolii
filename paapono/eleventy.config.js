ec838b60ac4e54da24a86dff31d37e536e2045fb
yisylvie <yisylvie1@gmail.com>
2026-08-30 00:44:56 -1000

    base site template seems to be working

--- paapono/eleventy.config.js
+++ paapono/eleventy.config.js
@@ -1,11 +1,51 @@
+import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
+import pluginWebc from "@11ty/eleventy-plugin-webc";
+// import path from "node:path";
+// import fs from "node:fs";
+
 export default function (eleventyConfig) {
 	eleventyConfig.setInputDirectory('src');
 	eleventyConfig.setOutputDirectory('dist');
+	
 	// folders/files that don't get formatted and are just copied directly
 	eleventyConfig.addPassthroughCopy('src/css');
+	eleventyConfig.addPassthroughCopy('src/icons/favicon/**');
+
+	eleventyConfig.addPlugin(pluginWebc, {
+		// Glob to find no-import global components
+		// This path is relative to the project-root!
+		// The default value is shown:
+		components: "src/_includes/components/**/*.webc",
+	});
+
+	eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
+		urlPath: "images",
+		// outputDir: "images",
+		
+		// output image formats
+		formats: ["webp", "jpg"],
+
+		// output image widths
+		widths: ["auto", 424, 1680, 2048],
+
+		filenameFormat: function (id, src, width, format, options) {
+			const extension = path.extname(src);
+			const name = path.basename(src, extension);
+
+			return `${name}-${width}w.${format}`;
+		},
+
+		// optional, attributes assigned on <img> nodes override these values
+		htmlOptions: {
+			imgAttributes: {
+				loading: "lazy",
+				decoding: "async",
+			}
+		},
+	});
 }
 
 export const config = {
-	markdownTemplateEngine: 'njk',
-	htmlTemplateEngine: 'njk',
+	markdownTemplateEngine: 'webc',
+	htmlTemplateEngine: 'webc',
 };
---
+++ paapono/src/about/index.md
@@ -0,0 +1,6 @@
+---
+layout: layouts/default.webc
+# description: hello
+---
+
+<!-- !['close-up of a baby possum among ferns'](./images/Miloliʻi%20Last%20Fishing%20Village%20in%20Hawaiʻi%20Nei.jpg) -->
